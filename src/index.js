f1b1e0d07eba11f0265deaf61c0bf55403ba8488
IvannaNinja <ivanna.heraskina@gmail.com>
2024-01-24 06:27:05 -0800

    API

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovierating] = useState(0)
+// function Test() {
+//   const [movieRating, setMovierating] = useState(0)
 
-  return <div>
-    <StarRating color='blue' maxRating={10} onSetRating={setMovierating} />
-    <p>This movie was rated {movieRating} stars</p>
-    </div>
-}
+//   return <div>
+//     <StarRating color='blue' maxRating={10} onSetRating={setMovierating} />
+//     <p>This movie was rated {movieRating} stars</p>
+//     </div>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
-    <StarRating size={24} color='red' className='test' defaultRating={3} />
+    <App />
+    {/* <StarRating maxRating={5} message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    <StarRating size={24} color='red' className='test' defaultRating={3} /> */}
 
-    <Test />
+    {/* <Test /> */}
   </React.StrictMode>
 );
