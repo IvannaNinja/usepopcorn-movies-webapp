42f6b232d9fe5f0d7476fc946323d857bb8d1205
IvannaNinja <ivanna.heraskina@gmail.com>
2024-02-02 15:11:01 -0800

    Fixing errors after node.js 17 updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
-import { u } from "tar";
+// import { u } from "tar";
 
 /*
 const tempMovieData = [
